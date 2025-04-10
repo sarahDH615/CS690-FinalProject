b32734f5b7f1895a9ca6630404e3029b57888f75
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-04-10 00:08:42 +0000

    add tests for DataManager

--- LearningTrackerProject/LearningTracker.Test/DataManagerTest.cs
+++ LearningTrackerProject/LearningTracker.Test/DataManagerTest.cs
@@ -1,10 +1,24 @@
 namespace LearningTracker.Test;
+using LearningTracker;
 
 public class DataManagerTest
 {
+    DataManager testDataManager = new DataManager();
     [Fact]
-    public void Test1()
+    public void TestGetLearning()
     {
+        var res = testDataManager.GetLearning("Skill"); // dictionary will be empty because no skills will have been added to it
+        Assert.Empty(res);
+    }
 
+    [Fact]
+    public void TestFormatLearningsForScript()
+    {
+        // args: List<string> learnings, List<string> orderedHeaders
+        List<string> testLearnings = new List<string>{"testId1|||testvalue1", "testId2|||testvalue2"};
+        List<string> testOrderedHeaders = new List<string>{"testHeader1", "testHeader2"};
+        var res = testDataManager.FormatLearningsForScript(testLearnings, testOrderedHeaders); 
+        Assert.Equal(2, res.Keys.Count);
+        Assert.Equal("testvalue1", res["testId1"]["testHeader2"]);
     }
 }
