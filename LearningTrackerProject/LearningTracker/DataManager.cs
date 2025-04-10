3b32e358e7fb84c0f23c41dcb436a80abf5362d2
Sarah DH <72268326+sarahDH615@users.noreply.github.com>
2025-04-10 22:28:45 +0000

    changes for initial notes addition

--- LearningTrackerProject/LearningTracker/DataManager.cs
+++ LearningTrackerProject/LearningTracker/DataManager.cs
@@ -1,30 +1,34 @@
 namespace LearningTracker;
 
 public class DataManager{
-    public Dictionary<string, string> learningsFilenameDict = new Dictionary<string, string>{
+    public Dictionary<string, string> filenameDict = new Dictionary<string, string>{
         {"Skill", "Skills.txt"},
         {"Goal", "Goals.txt"},
-        {"Milestone", "Milestones.txt"}
+        {"Milestone", "Milestones.txt"},
+        {"Note", "Notes.txt"}
     };
-    public Dictionary<string, List<string>> learningsHeadersDict = new Dictionary<string, List<string>>{
+    public Dictionary<string, List<string>> headersDict = new Dictionary<string, List<string>>{
         {"Skill", new List<string>{"ID", "Name", "Description", "Status"}},
         {"Goal", new List<string>{"ID", "Name", "Description", "Status", "parentID"}},
-        {"Milestone", new List<string>{"ID", "Name", "Description", "Status", "parentID"}}
+        {"Milestone", new List<string>{"ID", "Name", "Description", "Status", "parentID"}},
+        {"Note", new List<string>{"ID", "Name", "Description", "Body"}}
     };
     DataIO dataIO = new DataIO();
     public List<string> Skills = new List<string>();
     public List<string> Goals = new List<string>();
     public List<string> Milestones = new List<string>();
+    public List<string> Notes = new List<string>();
 
     public DataManager(){
-        Skills = dataIO.GetOrCreateFile(learningsFilenameDict["Skill"], learningsHeadersDict["Skill"]);
-        Goals = dataIO.GetOrCreateFile(learningsFilenameDict["Goal"], learningsHeadersDict["Goal"]);
-        Milestones = dataIO.GetOrCreateFile(learningsFilenameDict["Milestone"], learningsHeadersDict["Milestone"]);
+        Skills = dataIO.GetOrCreateFile(filenameDict["Skill"], headersDict["Skill"]);
+        Goals = dataIO.GetOrCreateFile(filenameDict["Goal"], headersDict["Goal"]);
+        Milestones = dataIO.GetOrCreateFile(filenameDict["Milestone"], headersDict["Milestone"]);
+        Notes = dataIO.GetOrCreateFile(filenameDict["Note"], headersDict["Note"]);
     }
 
     public Dictionary<string, Dictionary<string, string>> GetLearning(string learningType){
-        List<string> learningsList = dataIO.GetOrCreateFile(learningsFilenameDict[learningType], learningsHeadersDict[learningType]);
-        return FormatLearningsForScript(learningsList, learningsHeadersDict[learningType]);
+        List<string> learningsList = dataIO.GetOrCreateFile(filenameDict[learningType], headersDict[learningType]);
+        return FormatForScript(learningsList, headersDict[learningType]);
     }
 
     public string GenerateLearningID(string learningType){
@@ -41,7 +45,20 @@ public class DataManager{
         return count.ToString();
     }
 
-    public Dictionary<string, Dictionary<string, string>> FormatLearningsForScript(List<string> learnings, List<string> orderedHeaders){
+    public string CreateNoteID(List<string>idComponents, int componentLength=3){
+        List<string> components = new List<string>();
+        for(int segment = 0; segment < componentLength; segment++){
+            if(idComponents.Count > segment){
+                components.Add(idComponents[segment]);
+            }
+            else{
+                components.Add("xx");
+            }
+        }
+        return string.Join("-", components);
+    }
+
+    public Dictionary<string, Dictionary<string, string>> FormatForScript(List<string> learnings, List<string> orderedHeaders){
         Dictionary<string, Dictionary<string, string>> learningsDict = new Dictionary<string, Dictionary<string, string>>{};
         foreach(string learning in learnings){
             List<string> learningSplit = learning.Split("|||").ToList();
@@ -60,10 +77,10 @@ public class DataManager{
     public void SaveLearning(string learningType, Dictionary<string, string> learningDict){
         // create id for dict based on type
         string learningID = GenerateLearningID(learningType);
-        List<string> learningHeaders = learningsHeadersDict[learningType];
-        string learningFileName = learningsFilenameDict[learningType];
+        List<string> learningHeaders = headersDict[learningType];
+        string learningFileName = filenameDict[learningType];
         
-        string learningString = FormatLearningForDataStore(learningID, learningDict, learningHeaders);
+        string learningString = FormatForDataStore(learningID, learningDict, learningHeaders);
         // update the learnings lists
         if(learningType == "Skill"){
             Skills.Add(learningString);
@@ -78,11 +95,25 @@ public class DataManager{
         dataIO.SaveRecord(learningFileName, learningString);
     }
 
-    public string FormatLearningForDataStore(string id, Dictionary<string, string> recordDict, List<string> headers){
+    public void SaveNote(Dictionary<string, string> noteMetadata, List<string> idComponents){
+        // create id for dict based on type
+        string noteId = CreateNoteID(idComponents);
+        List<string> noteHeaders = headersDict["Note"];
+        string noteFileName = filenameDict["Note"];
+        
+        string noteString = FormatForDataStore(noteId, noteMetadata, noteHeaders);
+        // Console.WriteLine(noteString);
+        // update the notes list
+        Notes.Add(noteString);
+        // save back to data store
+        dataIO.SaveRecord(noteFileName, noteString);
+    }
+
+    public string FormatForDataStore(string id, Dictionary<string, string> recordDict, List<string> headers){
         
         List<string> recordList = new List<string>();
         foreach(string header in headers){ 
-            if(header == "ID"){
+            if(header == "ID" && recordDict.ContainsKey("ID") is false){
                 recordList.Add(id);
             }
             else{
